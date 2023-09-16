2709a74e069c43c44cbed34f8df7424b6d3d1f54
dr-zieba <zieba1jan@gmail.com>
2023-09-16 22:16:52 +0200

    v1

--- skrypt.py
+++ skrypt.py
@@ -99,24 +99,3 @@ def subtract(customer_id, amount_to_substract, table_name):
             con.close()
     except Exception as e:
         print(e)
-
-if __name__ == "__main__":
-    while True:
-        sql_fetch_all_data("Customer")
-        user_input = int(
-            input(
-                """What's your action?: 
-        1: Add
-        2: Subtract
-        3: Exit
-        """
-            )
-        )
-        customer_id_input = int(input("Insert customer ID: "))
-        amount_input = int(input("Insert amount to be added: "))
-        if user_input == 1:
-            add(customer_id_input, amount_input, table_name)
-        elif user_input == 2:
-            subtract(customer_id_input, amount_input, table_name)
-        elif user_input == 3:
-            exit()
